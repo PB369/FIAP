ee2678a35daf2084c5e598be331fb623a2c93895
PB369 <pedrohbarros369@gmail.com>
2023-08-05 14:53:03 -0300

    Jogo da Forca

--- Computational Thinking With Python/Exercícios/jogo-forca.py
+++ Computational Thinking With Python/Exercícios/jogo-forca.py
@@ -1,40 +1,61 @@
-# import random
+import random
 
-# indice_escondida = 0
-# palavra_escolhida = ""
-# frases = ["finge que tem, exige dos outros, mas não faz o que diz", "burro, burro, você é burro", "invariável, persistente"]
-# sorteio = random.randint(0, 3)
-# match sorteio:
-#     case 0:
-#         palavra_escolhida = "hipócrita"
-#     case 1:
-#         palavra_escolhida = "ignorante"
-#     case 2:
-#         palavra_escolhida = "constante"
-
-# palavra_escondida = ["_", "_", "_", "_", "_", "_", "_", "_", "_"]
-
-# print("Bem-vindo ao Jogo da Forca!")
-# print("estrutura da forca")
-# print(palavra_escondida)
-# letra = input("Digite seu palpite de letra: ")
-
-# for i in palavra_escolhida:
-#     indice_escondida += 1
-#     if letra == i:
-#         palavra_escondida.pop(indice_escondida)
-#         palavra_escondida.insert(indice_escondida, letra)
-#     else:
-#         print("Você errou!")
-
-lista = ["_", "_", "_", "_", "_"]
-string = "teste"
+tentativas = 6
 indice = 0
-for i in string:
-    letra = input("Digite a letra: ")
-    if letra == i:
-        lista.insert(indice, letra)
-        lista.pop()
-    print(lista)
-    indice += 1
-print(lista)
+vitoria = False
+
+sorteio_palavra = random.randint(1,3)
+match sorteio_palavra:
+    case 1:
+        palavra_secreta = "hipócrita"
+        dica = "finge que tem, exige dos outros, mas não faz o que fala"
+    case 2:
+        palavra_secreta = "ignorante"
+        dica = "burro, burro, você é burro!"
+    case 3:
+        palavra_secreta = "constante"
+        dica = "permanece firme; não muda"
+
+print(palavra_secreta)
+
+print("Bem-vindo ao Jogo da Forca!\n")
+
+forca_original = """
+----------
+         |"""
+
+cabeca_corpo = "\t O"
+tronco_corpo = "|"
+bra_dir_corpo = "\ "
+bra_esq_corpo = "\t/"
+perna_dir_corpo = " \ "
+perna_esq_corpo = "\t/"
+
+corpo = [perna_esq_corpo, perna_dir_corpo, bra_esq_corpo, bra_dir_corpo, tronco_corpo, cabeca_corpo]
+
+palavra_escondida = ["_", "_", "_", "_", "_", "_", "_", "_", "_"]
+
+print(f"\nTentativas Restantes: {tentativas}\n")
+
+while not tentativas == 0:
+    letra = input("Digite o seu palpite: \n")
+    if not (letra in palavra_secreta):
+        print("\nVocê errou! Tente de novo...\n")
+        tentativas -= 1
+        print(f"\nTentativas Restantes: {tentativas}\n")
+    else:
+        for i in palavra_secreta:
+            if letra == i:
+                palavra_escondida[indice] = letra
+            indice += 1
+        print(palavra_escondida)
+    indice = 0
+
+    if not ("_" in palavra_escondida) and tentativas > 0:
+        vitoria = True
+        break
+
+if vitoria == True:
+    print(f"\nParabéns! Você acertou a palavra secreta: {palavra_secreta}.")
+else:
+    print(f"\nVocê perdeu. A palavra secreta era '{palavra_secreta}'.")
